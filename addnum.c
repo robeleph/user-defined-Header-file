6dda6f90df6d299af77702e32840f109cc4cd9d0
Eyoel Wendwosen <eyu123med@gmail.com>
2018-05-31 22:09:44 +0300

    Initial Commit

---
+++ addnum.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "addnum.h"
+
+void main(){
+int sum = add(10,10);
+printf("The sum is: %d\n", sum);
+}
---
+++ addnum.h
@@ -0,0 +1,9 @@
+#ifndef ADDNUM_H_INCLUDED
+#define ADDNUM_H_INCLUDED
+
+int add(int a, int b){
+	return(a+b);
+
+
+}
+#endif
